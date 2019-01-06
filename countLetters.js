f28df84498b4037b962ad3bb81780007a8ffa863
Richmond Wong <24490829+richmondwong@users.noreply.github.com>
2019-01-06 17:17:35 -0500

    Update countLetters.js

--- countLetters.js
+++ countLetters.js
@@ -2,13 +2,9 @@ var output = {}
 
 var letters = "abcdefgh"
 
-
 function countLetters(str){
-
-
-for (var i = 0; i < str.length; i++){
-  output[str[i]] += 1
-}
-console.log(output)
-
+  for (var i = 0; i < str.length; i++){
+    output[str[i]] += 1
+  }
+  console.log(output)
 }
